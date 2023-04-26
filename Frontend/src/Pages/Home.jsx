cf050288eb75e367cc857744d3aeb2e3bfc8d8d3
Rahulyh010 <110051547+Rahulyh010@users.noreply.github.com>
2023-04-26 22:52:46 +0530

    added gif

--- Frontend/src/Pages/Home.jsx
+++ Frontend/src/Pages/Home.jsx
@@ -5,7 +5,9 @@ export const Home = () => {
   return (
     <div>
         <Navbar/>
-       
+       <div>
+        <img src="https://gifdb.com/images/high/you-re-welcome-bird-de2nttnl8ffq7gbo.gif" alt="" />
+       </div>
     </div>
   )
 }
