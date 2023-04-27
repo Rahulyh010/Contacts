90934e4534ed39781440b7f507600aafd032b4f5
Rahulyh010 <110051547+Rahulyh010@users.noreply.github.com>
2023-04-27 05:58:06 +0530

    formated-with-prettier

--- Frontend/src/Components/Navbar.jsx
+++ Frontend/src/Components/Navbar.jsx
@@ -1,13 +1,19 @@
-import React from 'react'
-import "../Styles/navbar.css"
-import { Link } from 'react-router-dom'
+import React from "react";
+import "../Styles/navbar.css";
+import { Link } from "react-router-dom";
 
 export const Navbar = () => {
   return (
     <div id="navbar">
-        <h1><Link to="/">Home</Link></h1>
-        <h1><Link to="/contacts">Contacts</Link> </h1>
-        <h1><Link to="/AddContact" >Add Contact</Link></h1>
+      <h1>
+        <Link to="/">Home</Link>
+      </h1>
+      <h1>
+        <Link to="/contacts">Contacts</Link>{" "}
+      </h1>
+      <h1>
+        <Link to="/AddContact">Add Contact</Link>
+      </h1>
     </div>
-  )
-}
+  );
+};
--- Frontend/src/Pages/Home.jsx
+++ Frontend/src/Pages/Home.jsx
@@ -1,13 +1,16 @@
-import React from 'react'
-import { Navbar } from '../Components/Navbar'
+import React from "react";
+import { Navbar } from "../Components/Navbar";
 
 export const Home = () => {
   return (
     <div>
-        <Navbar/>
-       <div>
-        <img src="https://gifdb.com/images/high/you-re-welcome-bird-de2nttnl8ffq7gbo.gif" alt="" />
-       </div>
+      <Navbar />
+      <div>
+        <img
+          src="https://gifdb.com/images/high/you-re-welcome-bird-de2nttnl8ffq7gbo.gif"
+          alt=""
+        />
+      </div>
     </div>
-  )
-}
+  );
+};
